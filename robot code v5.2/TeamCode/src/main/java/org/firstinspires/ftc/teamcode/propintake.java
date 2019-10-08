8361ee474d52d2ef12cad9d06e0458ba8c758d23
Black Tigers (Dell) <blacktigersftc@gmail.com>
2019-10-08 11:15:33 +0300

    intake movement and servoes
    Signed-off-by: Black Tigers (Dell) <blacktigersftc@gmail.com>

--- robot code v5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/propintake.java
+++ robot code v5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/propintake.java
@@ -35,6 +35,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Demonstrates empty OpMode
@@ -42,67 +43,97 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name = "intake", group = "Concept")
 public class propintake extends OpMode {
 
-  private ElapsedTime runtime = new ElapsedTime();
-  private DcMotor motor = null;
-  private Servo servoLeft = null;
-  private Servo servoRight = null;
-  private double power = 0.5;
-  private double upPos = 1;
-  private double downPos = 0;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motor = null;
+    private Servo servoLeft = null;
+    private Servo servoRight = null;
+    private double power = 0.5;
+    private double upPos = 1;
+    private double downPos = 0;
 
-  @Override
-  public void init() {
-    telemetry.addData("Status", "Initialized");
-    motor = hardwareMap.get(DcMotor.class, "Motor");
-    servoLeft = hardwareMap.get(Servo.class, "ServoLeft");
-    servoRight = hardwareMap.get(Servo.class, "ServoRight");
-    servoLeft.setDirection(Servo.Direction.FORWARD);
-    servoRight.setDirection(Servo.Direction.FORWARD);
-  }
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        motor = hardwareMap.get(DcMotor.class, "Motor");
+        servoLeft = hardwareMap.get(Servo.class, "ServoLeft");
+        servoRight = hardwareMap.get(Servo.class, "ServoRight");
+        servoLeft.setDirection(Servo.Direction.FORWARD);
+        servoRight.setDirection(Servo.Direction.FORWARD);
+    }
 
-  /*
+    /*
      * Code to run when the op mode is first enabled goes here
      * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
      */
-  @Override
-  public void init_loop() {
-  }
-
-  /*
-   * This method will be called ONCE when start is pressed
-   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-   */
-  @Override
-  public void start() {
-    runtime.reset();
-  }
-
-  /*
-   * This method will be called repeatedly in a loop
-   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-   */
-  @Override
-  public void loop() {
-    telemetry.addData("Status", "Run Time: " + runtime.toString());
-    //motor = gamepad1.right_trigger;
-    if (gamepad1.right_trigger!=0){
-      motor.setPower(power);
-    }
-    else{
-      motor.setPower(0);
-    }
-    if (gamepad1.y){
-      servoRight.setPosition(upPos);
-      servoLeft.setPosition(upPos);
-    }
-    else if  (gamepad1.a){
-      servoRight.setPosition(downPos);
-      servoLeft.setPosition(downPos);
+    @Override
+    public void init_loop() {
     }
 
+    /*
+     * This method will be called ONCE when start is pressed
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
 
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    boolean dUpPrev;
+    boolean dDownPrev;
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        boolean dUp=gamepad1.dpad_up;
+        boolean dDown=gamepad1.dpad_down;
+        if (gamepad1.right_trigger != 0) {
+            motor.setPower(power);
+            if (dUp&&!dUpPrev) {
+                    power = power + 0.1;
+                    power = Range.clip(power, 0.1, 1);
+                } else if (dDown&&!dDownPrev) {
+                    power = power - 0.1;
+                    power = Range.clip(power, 0.1, 1);
+            }
 
+        }
+        if (gamepad1.left_trigger != 0) {
+            motor.setPower(-power);
+            if (dUp&&!dUpPrev) {
+                power = power + 0.1;
+                power = Range.clip(power, 0.1, 1);
+            }
+            else if (dDown&&!dDownPrev) {
+                power = power - 0.1;
+                power = Range.clip(power, 0.1, 1);
+            }
+        }
+        else {
+            motor.setPower(0);
+        }
+        if (gamepad1.y) {
+            servoRight.setPosition(upPos);
+            servoLeft.setPosition(upPos);
+            if (dUp&&!dUpPrev) {
+                upPos = upPos + 0.1;
+                upPos = Range.clip(upPos, 0.1, 1);
+            } else if (dDown&&!dDownPrev) {
+                upPos = upPos - 0.1;
+                upPos = Range.clip(upPos, 0.1, 1);
 
-  }
+            }
+        } else if (gamepad1.a) {
+            servoRight.setPosition(downPos);
+            servoLeft.setPosition(downPos);
+        }
+        telemetry.addData("power", power);
+        telemetry.addData("position", upPos);
+
+        dUpPrev = dUp;
+        dDownPrev = dDown;
+    }
 }
 
